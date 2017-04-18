75abb71f27c519bddae6df91eae885e405ac0598
JaimeMerino <jaime.merino@letsbonus.com>
2017-04-18 13:03:39 +0200

    fix main.go

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ ADD . /go/src/hello
 RUN go install hello
 ENTRYPOINT /go/bin/basic_web_server
  
-EXPOSE 8080
-
-
+EXPOSE 8083
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
  
 import(
-"fmt"
-"net/http"
-"runtime"
+	"fmt"
+	"net/http"
+	"runtime"
 )
  
 func indexHandler( w http.ResponseWriter, r *http.Request){
-fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
- 
+	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
+}
+
+func pingHandler( w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
 }
  
 func main(){
-http.HandleFunc("/", indexHandler)
-http.ListenAndServe(":8080",nil)
-
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.ListenAndServe(":8080",nil)
+}
